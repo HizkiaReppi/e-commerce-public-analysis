db6206f0d7d85428202f096519f57d2cad4cd599
Hizkia Reppi <hizkiajefren0501@gmail.com>
2023-12-25 17:50:42 +0800

    Setup Project

---
+++ dashboard/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import streamlit as st
+import urllib
+
+st.write("Hello World")
+
+# Copyright 2023 Hizkia Jefren Reppi
